ebf5a693820fb7b15f32e70f2e022639d7be9dee
Mauricio Neto <maumauneto22@gmail.com>
2026-05-26 10:47:37 -0300

    Cria admin automático

--- run.py
+++ run.py
@@ -1,10 +1,28 @@
 from app import create_app, db
+from app.models.user import User
+from werkzeug.security import generate_password_hash
 
 app = create_app()
 
 with app.app_context():
+
     db.create_all()
 
-if __name__ == '__main__':
+    admin = User.query.filter_by(username='admin').first()
 
-    app.run(debug=False)
+    if not admin:
+
+        admin = User(
+            username='admin',
+            password=generate_password_hash('admin123'),
+            tipo='admin',
+            ativo=True
+        )
+
+        db.session.add(admin)
+        db.session.commit()
+
+        print('Usuário admin criado!')
+
+if __name__ == '__main__':
+    app.run(debug=True)
